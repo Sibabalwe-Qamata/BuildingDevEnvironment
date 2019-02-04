3fe8469d54feb7010db4f7e2847446d69e4fefc0
Sibabalwe-Qamata <prof.qamata@gmail.com>
2019-02-04 19:34:03 +0200

    made updates

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -12,18 +15,44 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
-     //Eliminate duplicate packages when generating a bundle
+       // Hash the files using MD5 so that their names change when the content changes.
+      new WebpackMd5Hash(),
+      // Generate an external css file with a hash in the filename
+      new ExtractTextPlugin('[name].[contenthash].css'),
+
+      // Create HTML file that includes reference to bundled JS.
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
+        },
+        inject: true,
+        // Properties you define here are available in index.html
+        // using htmlWebpackPlugin.options.varName
+        trackJSToken: 'INSERT YOUR TOKEN HERE'
+      }),
+      //Eliminate duplicate packages when generating a bundle
       new webpack.optimize.DedupePlugin(),
       //Minify JS
-      new webpack.optimize.UglifyJsPlugin()
+      new webpack.optimize.UglifyJsPlugin(),
+
   ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loaders: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
